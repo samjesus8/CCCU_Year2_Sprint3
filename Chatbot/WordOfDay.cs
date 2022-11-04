2b6f3b9fda3c4a828d2f564712815f29e782ba49
IU14 <iu14@canterbury.ac.uk>
2022-11-04 20:38:20 +0000

    working on word of the day API crated class and attributes. started to create method. found API online to use but having trouble implementing curently

---
+++ Chatbot/WordOfDay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chatbot
+{
+    internal class WordOfDay
+    {
+        public class word
+        {
+            public string wordText { get; set; }
+            public string wordDefinition { get; set; }
+        }
+    }
+}
--- Chatbot/keywordChecker.cs
+++ Chatbot/keywordChecker.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -54,6 +55,65 @@ namespace Chatbot
             else if (input.ToLower().Contains("wind")) // if the user mentions wind this shall be outputted
             {
                 outputBox.Text = "The Wind Speed in Canterbury is - " + GetWeather().wind.speed.ToString() + "Mph \r\nThe Wind Degrees is at - " + GetWeather().wind.deg.ToString() + "\r\nGusts of Upto - " + GetWeather().wind.gust.ToString() + "Mph";
+            }
+        }
+
+        public WordOfDay.word GetWord() // gets the current word of the day using the API
+        {
+            WordOfDay.word word;
+
+            word = new WordOfDay.word();
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://word-of-the-day2.p.rapidapi.com/word/today"),
+            };
+                     
+            return word;
+
+            ///<summary>
+            /// below is the code from the APi website but I am unsure how to implement it. There's no tutorials really on how to code this that I can find. 
+            /// above have based it on the code for the weather. 
+            /// To do more research
+            /// </summary>
+
+            /*
+             * var client = new HttpClient();
+               var request = new HttpRequestMessage
+            {
+	               Method = HttpMethod.Get,
+	               RequestUri = new Uri("https://word-of-the-day2.p.rapidapi.com/word/today"),
+	              Headers =
+	                {
+		                { "X-RapidAPI-Key", "e849bcc6aemsh88ac2ae09d02290p196cf4jsn3f93c10f3361" },
+		                { "X-RapidAPI-Host", "word-of-the-day2.p.rapidapi.com" },
+	                },
+            };
+                using (var response = await client.SendAsync(request))
+              {
+	                  response.EnsureSuccessStatusCode();
+	                   var body = await response.Content.ReadAsStringAsync();
+	              Console.WriteLine(body);
+                  }
+            */
+            
+        }
+
+        public void WOTDInput(string input, TextBox outputBox)
+        {
+            if (input.ToLower().Contains("word of the day")) // of user asks this this will be outputted
+            {
+                outputBox.Text = "The word of the day is: " + GetWord().wordText + "\r\nDefinition: " + GetWord().wordDefinition; 
+            }
+            else if (input.ToLower().Contains("wotd"))
+            {
+                outputBox.Text = "The word of the day is: " + GetWord().wordText + "\r\nDefinition: " + GetWord().wordDefinition;
+            }
+            else if (input.ToLower().Contains("Todays word"))
+            {
+                outputBox.Text = "The word of the day is: " + GetWord().wordText + "\r\nDefinition: " + GetWord().wordDefinition;
             }
         }
     }
