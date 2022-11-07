b510219caaae6289bef7703610cda484805e80f6
Samuel Jesuthas <samueljesuthas27@gmail.com>
2022-11-07 14:41:24 +0000

    Issue #1 - Created Music Player Foundation
    -Created MusicPlayer.cs
    -Added a simple Now Playing box and some buttons for functionality

---
+++ Chatbot/Music/MusicPlayer.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Chatbot.Music
+{
+    partial class MusicPlayer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nowPlayingBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.playButton = new System.Windows.Forms.Button();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.loopButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("NSimSun", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(132, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Music Player";
+            // 
+            // nowPlayingBox
+            // 
+            this.nowPlayingBox.Location = new System.Drawing.Point(12, 84);
+            this.nowPlayingBox.Name = "nowPlayingBox";
+            this.nowPlayingBox.ReadOnly = true;
+            this.nowPlayingBox.Size = new System.Drawing.Size(477, 23);
+            this.nowPlayingBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Now Playing";
+            // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(115, 127);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(61, 45);
+            this.playButton.TabIndex = 3;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Location = new System.Drawing.Point(184, 127);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(61, 45);
+            this.pauseButton.TabIndex = 4;
+            this.pauseButton.Text = "Pause";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(251, 127);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(61, 45);
+            this.stopButton.TabIndex = 5;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // loopButton
+            // 
+            this.loopButton.Location = new System.Drawing.Point(318, 127);
+            this.loopButton.Name = "loopButton";
+            this.loopButton.Size = new System.Drawing.Size(61, 45);
+            this.loopButton.TabIndex = 6;
+            this.loopButton.Text = "Loop";
+            this.loopButton.UseVisualStyleBackColor = true;
+            this.loopButton.Click += new System.EventHandler(this.loopButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(386, 242);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "V0.1 ALPHA";
+            // 
+            // MusicPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 266);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.loopButton);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nowPlayingBox);
+            this.Controls.Add(this.label1);
+            this.Name = "MusicPlayer";
+            this.Text = "MusicPlayer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox nowPlayingBox;
+        private Label label2;
+        private Button playButton;
+        private Button pauseButton;
+        private Button stopButton;
+        private Button loopButton;
+        private Label label3;
+    }
+}
---
+++ Chatbot/Music/MusicPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chatbot.Music
+{
+    public partial class MusicPlayer : Form
+    {
+        public MusicPlayer()
+        {
+            InitializeComponent();
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loopButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
